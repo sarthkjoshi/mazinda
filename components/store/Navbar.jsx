ab007b8979eda8df7cdc10545dd94976a296fbdc
Abhey Gupta <abheygupta1731@gmail.com>
2023-11-03 19:51:01 +0530

    bug fixes

--- components/store/Navbar.jsx
+++ components/store/Navbar.jsx
@@ -1,28 +1,25 @@
 import Link from "next/link";
-import MazindaLogo from "@/public/logo_mazinda.png";
-import Image from "next/image";
-import { usePathname } from "next/navigation";
+import MazindaLogo from '@/public/logo_mazinda.png'
+import Image from 'next/image'
 
 const Navbar = () => {
-  const pathname = usePathname();
   return (
     <>
-      {!pathname.includes("auth") && (
-        <nav className="bg-white border-gray-200 dark:bg-gray-900 py-2">
-          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <Link href="/store" className="flex items-center">
-              <Image
-                width={120}
-                height={60}
-                src={MazindaLogo}
-                className="h-8 mr-3"
-                alt="Mazinda Logo"
-              />
-            </Link>
-          </div>
-        </nav>
-      )}
-    </>
+    {<nav className="bg-white border-gray-200 dark:bg-gray-900 py-2">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link href="/store" className="flex items-center">
+          <Image
+          width={120}
+          height={60}
+          src={MazindaLogo}
+          className="h-8 mr-3"
+          alt="Mazinda Logo"
+          />
+        </Link>
+        
+      </div>
+    </nav>}
+          </>
   );
 };
 
