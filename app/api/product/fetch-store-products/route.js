1864bc1a3c41fa8bd3f1da5036ceaada0e613d3d
Abhey Gupta <abheygupta1731@gmail.com>
2023-12-23 21:54:01 +0530

    made api to fetch products of a store

--- app/api/product/fetch-store-products/route.js
+++ app/api/product/fetch-store-products/route.js
@@ -1,4 +1,5 @@
 import Store from "@/models/Store";
+import Product from "@/models/Product";
 import connectDB from "@/lib/mongoose";
 import { NextResponse } from "next/server";
 
@@ -9,15 +10,18 @@ export async function POST(req) {
         // Connecting to database
         await connectDB();
 
-        // Checking if the Vendor already exists
+        // Checking if the Store exists
         let store = await Store.findById(storeId).select('-mobileNumber -alternateMobileNumber -password')
 
         if (store) {
-            return NextResponse.json({ success: true, message: "Store fetched successfully", store });
+            // Fetching products with the specified storeId
+            let products = await Product.find({ storeId });
+
+            return NextResponse.json({ success: true, message: "Products of the store fetched successfully", products });
         } else {
             return NextResponse.json({ success: false, message: "Store doesn't exist" });
         }
     } catch (error) {
-        return NextResponse.json({ success: false, error: "An error occurred while fetching the store : " + error });
+        return NextResponse.json({ success: false, error: "An error occurred while fetching the store and products: " + error });
     }
 }
--- models/Store.js
+++ models/Store.js
@@ -9,6 +9,7 @@ const StoreSchema = new mongoose.Schema({
     password: { type: String, required: true },
     storeAddress: { type: Object, required: true },
     category: { type: String },
+    followers: { type: Array },
     openStatus: { type: Boolean, default: true },
     approvedStatus: { type: String, default: "pending" }
 }, { timestamps: true });
