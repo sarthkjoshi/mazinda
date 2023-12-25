55a7865a391972a7e0ebf1a7cc9a0302ff436d91
Abhey Gupta <abheygupta1731@gmail.com>
2023-12-25 19:17:09 +0530

    2

--- app/api/auth/credentials-in-use/route.js
+++ app/api/auth/credentials-in-use/route.js
@@ -3,16 +3,21 @@ import connectDB from "@/lib/mongoose";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-    const { phone_number } = await req.json();
+    const { email, phone_number } = await req.json();
 
     try {
         // Connecting to the database
         await connectDB();
 
-        let user = await User.findOne({ mobileNumber: phone_number });
+        const userByEmail = await User.findOne({ email });
+        const userByPhoneNumber = await User.findOne({ mobileNumber: phone_number });
 
-        if (user) {
-            return NextResponse.json({ success: true, usedStatus: true });
+        if (userByEmail && userByPhoneNumber) {
+            return NextResponse.json({ success: true, usedStatus: true, message: 'both' });
+        } else if (userByEmail) {
+            return NextResponse.json({ success: true, usedStatus: true, message: 'email' });
+        } else if (userByPhoneNumber) {
+            return NextResponse.json({ success: true, usedStatus: "phone_number", message: 'phone_number' });
         } else {
             return NextResponse.json({ success: true, usedStatus: false });
         }
