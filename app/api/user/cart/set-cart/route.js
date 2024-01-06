7834d4601798112ee1f504dffa79752f0988fb2c
Abhey Gupta <abheygupta1731@gmail.com>
2024-01-07 00:32:28 +0530

    minor api change

--- app/api/user/cart/set-cart/route.js
+++ app/api/user/cart/set-cart/route.js
@@ -1,19 +1,15 @@
 import User from "@/models/User";
 import connectDB from "@/lib/mongoose";
 import { NextResponse } from "next/server";
-import jwt from "jsonwebtoken";
 
 export async function POST(req) {
-  const { userToken, newCart } = await req.json();
-
-  // Verify the user's token to get their email
-  const userData = jwt.verify(userToken, "this is jwt secret");
+  const { userId, newCart } = await req.json();
 
   try {
     await connectDB();
 
     // Find the user by their email
-    let user = await User.findOne({ email: userData.email });
+    let user = await User.findById(userId);
 
     if (user) {
       user.cart = newCart;
