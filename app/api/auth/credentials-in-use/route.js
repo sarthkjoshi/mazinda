a92d21184aef3061964450fb7c65065a99cbd91b
Abhey Gupta <abheygupta1731@gmail.com>
2024-01-09 20:59:53 +0530

    modifications to auth api

--- app/api/auth/credentials-in-use/route.js
+++ app/api/auth/credentials-in-use/route.js
@@ -3,25 +3,55 @@ import connectDB from "@/lib/mongoose";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-    const { email, phone_number } = await req.json();
+  const { email, phone_number, checkEmail, checkPhoneNumber } =
+    await req.json();
 
-    try {
-        // Connecting to the database
-        await connectDB();
+  try {
+    // Connecting to the database
+    await connectDB();
 
-        const userByEmail = await User.findOne({ email });
-        const userByPhoneNumber = await User.findOne({ phoneNumber: phone_number });
-
-        if (userByEmail && userByPhoneNumber) {
-            return NextResponse.json({ success: true, usedStatus: true, message: 'User already exists' });
-        } else if (userByEmail) {
-            return NextResponse.json({ success: true, usedStatus: true, message: 'Email already associated with another account' });
-        } else if (userByPhoneNumber) {
-            return NextResponse.json({ success: true, usedStatus: "phone_number", message: 'Phone Number already associated with another account' });
-        } else {
-            return NextResponse.json({ success: true, usedStatus: false });
-        }
-    } catch (error) {
-        return NextResponse.json({ success: false, error: "An error occurred: " + error });
+    if ((checkEmail && !email) || (checkPhoneNumber && !phone_number)) {
+      return NextResponse.json({
+        success: false,
+        error: "Email or Phone Number is required for checking",
+      });
     }
+
+    let userByEmail, userByPhoneNumber;
+
+    if (checkEmail) {
+      userByEmail = await User.findOne({ email });
+    }
+
+    if (checkPhoneNumber) {
+      userByPhoneNumber = await User.findOne({ phoneNumber: phone_number });
+    }
+
+    if (userByEmail && userByPhoneNumber) {
+      return NextResponse.json({
+        success: true,
+        usedStatus: true,
+        message: "User already exists",
+      });
+    } else if (userByEmail) {
+      return NextResponse.json({
+        success: true,
+        usedStatus: true,
+        message: "Email already associated with another account",
+      });
+    } else if (userByPhoneNumber) {
+      return NextResponse.json({
+        success: true,
+        usedStatus: "phone_number",
+        message: "Phone Number already associated with another account",
+      });
+    } else {
+      return NextResponse.json({ success: true, usedStatus: false });
+    }
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      error: "An error occurred: " + error,
+    });
+  }
 }
