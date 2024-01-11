eb96ea1b1c7c41bda19e82cfa36fc81fbfc905aa
Abhey Gupta <abheygupta1731@gmail.com>
2024-01-11 12:54:00 +0530

    modified whatsapp api

--- app/api/whatsapp/msg-to-group/route.js
+++ app/api/whatsapp/msg-to-group/route.js
@@ -1,17 +1,18 @@
 import { NextResponse } from "next/server";
-import axios from 'axios'
+import axios from "axios";
 
 export async function POST(req) {
-    // const {  } = await req.json()
+  // const {  } = await req.json()
 
-    const message = `New Order On Mazinda!`;
+  const message = `New Order On Mazinda!`;
 
-    const { data } = await axios.post(`https://wapp.powerstext.in/api/send_group?group_id=120363190214054720@g.us&type=text&message=${message}&instance_id=6534CC8282DA7&access_token=65338e1dbc831`);
+  const { data } = await axios.post(
+    `https://wapp.powerstext.in/api/send_group?group_id=120363218361761258@g.us&type=text&message=${message}&instance_id=6534CC8282DA7&access_token=65338e1dbc831`
+  );
 
-    if (data.status === 'success') {
-        return NextResponse.json({ status: 200, success: true })
-    }
+  if (data.status === "success") {
+    return NextResponse.json({ status: 200, success: true });
+  }
 
-    return NextResponse.json({ status: 400, success: false, response })
-
+  return NextResponse.json({ status: 400, success: false, response });
 }
