a990f099cf9446b8a91b9a68515dcd097e6dd011
Abhey Gupta <abheygupta1731@gmail.com>
2024-01-13 17:12:57 +0530

    Added api for creating and fetching food orders

---
+++ app/api/order/create-food-order/route.js
@@ -0,0 +1,83 @@
+// import Order from "@/models/Order";
+// import connectDB from "@/lib/mongoose";
+// import { NextResponse } from "next/server";
+// import jwt from "jsonwebtoken";
+// import User from "@/models/User";
+
+// export async function POST(req) {
+//   try {
+//     const { userToken, userCart, pricing, address, paymentMethod } =
+//       await req.json();
+
+//     const userData = jwt.verify(userToken, "this is jwt secret");
+
+//     await connectDB();
+
+//     let user = await User.findOne({ email: userData.email });
+//     if (!user) {
+//       return NextResponse.json({
+//         success: false,
+//         error: "User doesn't exists",
+//       });
+//     }
+
+//     await Order.create({
+//       userId: user._id,
+//       cart: userCart,
+//       address,
+//       pricing,
+//       paymentMethod,
+//     });
+//     return NextResponse.json({
+//       success: true,
+//       message: "Order placed successfully",
+//     });
+//   } catch (error) {
+//     return NextResponse.json({
+//       success: false,
+//       error: "An error occurred while creating the Order : " + error,
+//     });
+//   }
+// }
+
+import FoodOrder from "@/models/FoodOrder";
+import connectDB from "@/libs/mongoose";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  try {
+    const {
+      userId,
+      vendorId,
+      products,
+      address,
+      amount,
+      externalDeliveryRequired,
+      cutleryRequirement,
+      paymentInfo,
+      paymentMethod,
+    } = await req.json();
+    await connectDB();
+    const foodOrder = await FoodOrder.create({
+      userId,
+      vendorId,
+      products,
+      address,
+      amount,
+      externalDeliveryRequired,
+      cutleryRequirement,
+      paymentInfo,
+      paymentMethod,
+    });
+    return NextResponse.json({
+      success: true,
+      message: "Food order created successfully",
+      order: foodOrder,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      error: "An error occurred while creating the order : " + error,
+    });
+  }
+}
---
+++ models/FoodOrder.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const FoodOrderSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    vendorId: { type: String, required: true },
+    products: { type: Object, required: true },
+    address: { type: Object, required: true },
+    amount: { type: Number, required: true },
+    instructions: { type: String },
+    externalDeliveryRequired: { type: Boolean },
+    cutleryQuantity: { type: Number },
+    paymentInfo: { type: Object },
+    paymentMethod: { type: String },
+    status: { type: String, default: "Processing" },
+  },
+  { timestamps: true }
+);
+
+mongoose.models = {};
+export default mongoose.model("FoodOrder", FoodOrderSchema);
