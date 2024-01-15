4a1aa491a68eb9da600a0efec15945eeed76e516
Abhey Gupta <abheygupta1731@gmail.com>
2024-01-15 20:52:19 +0530

    fixed libs folder name

--- app/api/vendor/order/deleteorder/route.js
+++ app/api/vendor/order/deleteorder/route.js
@@ -1,14 +1,20 @@
 import Order from "@/models/Order";
-import connectDB from "@/libs/mongoose";
+import connectDB from "@/lib/mongoose";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-    try {
-        const { orderId } = await req.json();
-        await connectDB()
-        await Order.findByIdAndDelete(orderId)
-        return NextResponse.json({ success: true, message: "Order deleted successfully" });
-    } catch (error) {
-        return NextResponse.json({ success: false, error: "An error occurred while deleting the order : " + error });
-    }
+  try {
+    const { orderId } = await req.json();
+    await connectDB();
+    await Order.findByIdAndDelete(orderId);
+    return NextResponse.json({
+      success: true,
+      message: "Order deleted successfully",
+    });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      error: "An error occurred while deleting the order : " + error,
+    });
+  }
 }
