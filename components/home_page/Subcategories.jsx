69d28e110e1e30d1f1289325f0c41d7fd6adeb40
Deepak Dubey <deepak.gkpwebsolution@gmail.com>
2024-01-17 13:45:28 +0530

    fixed

--- components/home_page/Subcategories.jsx
+++ components/home_page/Subcategories.jsx
@@ -31,13 +31,27 @@ const renderSubcategory = ({ counter }) => {
 const Subcategories = () => {
 
   const selectedLocation = useLocation();
-  console.log("selectedLocation" , selectedLocation);
+  // console.log("selectedLocation" , selectedLocation);
   let show_food_cat = false;
   if(selectedLocation.city == "Mandi"){
     show_food_cat = true;
   }
+  const arrayToMap = show_food_cat ? [1, 2, 3] : [1, 2, 3, 4];
   return (
     <>
+      <div className="md:hidden grid  w-full ">
+        {show_food_cat && (
+              <div className=" overflow-hidden relative   rounded-md">
+                <Link href="/food">
+                    <img
+                        src={`https://mazindabucket.s3.ap-south-1.amazonaws.com/home-page/mandi_food_order.png`}
+                        alt=""
+                        className="border-gray-300 border-1 object-contain w-full h-full"
+                    />
+                </Link>
+              </div>
+          )}
+      </div>
       <div className="md:hidden grid grid-cols-2 gap-4 mt-2 justify-center">
         {[1, 2, 3, 4].map((counter) => (
           <React.Fragment key={counter}>
@@ -59,8 +73,8 @@ const Subcategories = () => {
                </Link>
             </div>
         )}
-
-        {[1, 2, 3, 4].map((counter) => (
+        
+        {arrayToMap.map((counter) => (
           <React.Fragment key={counter}>
             {renderSubcategory({ counter })}
           </React.Fragment>
