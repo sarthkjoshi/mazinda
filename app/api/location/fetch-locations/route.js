c2e17816095c8a37e55f8b79bc8a229b85ac3be2
Abhey Gupta <abheygupta1731@gmail.com>
2024-01-19 21:33:13 +0530

    api change

--- app/api/location/fetch-locations/route.js
+++ app/api/location/fetch-locations/route.js
@@ -2,12 +2,15 @@ import Location from "@/models/Location";
 import connectDB from "@/lib/mongoose";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-    try {
-        await connectDB()
-        let locations = await Location.find();
-        return NextResponse.json({ success: true, locations });
-    } catch (error) {
-        return NextResponse.json({ success: false, error: "An error occurred while fetching the Locations : " + error });
-    }
+export async function POST() {
+  try {
+    await connectDB();
+    let locations = await Location.find();
+    return NextResponse.json({ success: true, locations });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      error: "An error occurred while fetching the Locations : " + error,
+    });
+  }
 }
