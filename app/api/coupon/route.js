602700b970b6bf9a6d7e6291e46844ef5bb7040a
Abhey Gupta <abheygupta1731@gmail.com>
2024-01-23 21:01:48 +0530

    api changes

---
+++ app/api/coupon/route.js
@@ -0,0 +1,22 @@
+import Coupon from "@/models/Coupon";
+import connectDB from "@/lib/mongoose";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  const { code } = await req.json();
+  try {
+    await connectDB();
+    let coupon = await Coupon.find({ $match: { code } });
+
+    return NextResponse.json({
+      success: true,
+      message: "Coupon fetched successfully",
+      coupon,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      error: "An error occurred while creating the Coupon : " + error,
+    });
+  }
+}
