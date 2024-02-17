9a6f1abc49f397aef9cebabe62c60048083a901d
Abhey Gupta <abheygupta1731@gmail.com>
2024-02-17 11:37:00 +0530

    .

--- models/LookingFor.js
+++ models/LookingFor.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const LookingForSchema = new mongoose.Schema({
-    image: { type: String, required: true },
-    link_type: { type: String, required: true },
-    category_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: false },
-    cityIds: { type: Array, required: true },
+  image: { type: String, required: true },
+  link_type: { type: String, required: true },
+  category_id: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+  cityIds: { type: Array, required: true },
 });
 
-mongoose.models = {}
+mongoose.models = {};
 export default mongoose.model("LookingFor", LookingForSchema);
